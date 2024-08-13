6c6c73b7be380dd43211b689413715774105709b
Tlalemasego <tlalemasego@gmail.com>
2024-08-13 13:30:24 +0200

    git commit -  completed app setup.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+     
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
