3f8218a3005d5c8f474568b29b98c8d26b9040a4
Tlalemasego <tlalemasego@gmail.com>
2024-08-13 13:58:47 +0200

    git commit - fetched posts and implemented error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,29 @@ function App() {
   const [ posts, setPosts] = useState([]);
   const [ error, setError] = useSstate(null);
 
+useEffect(() => {
+  const fetchPosts = async ()=> {
+    try {
+      const response = await fetch ("https://jsonplaceholder.typicode.com/posts");
+      if (!response.ok) { 
+        throw new Error("Data fetching failed");
+      }
+      const data = await response.json();
+      setPosts(data);
+    } catch(error) {
+      setError(error.message);
+    }
+  };
+  fetchPosts();
+}, []);
 
 
-  
+
+
+
+
   return (
-    <>
+    <>t
       <div>
      
       </div>
